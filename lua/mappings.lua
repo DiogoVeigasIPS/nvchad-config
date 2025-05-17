75da509c56541b224ccf6635f975a11af8991e39
DiogoVeigasIPS <202200879@estudantes.ips.pt>
2025-05-17 10:20:31 +0100

    Update nvchad dashboard

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "Toggle Zen Mode" })
+map('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP Code Action' })
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- lua/options.lua
+++ lua/options.lua
@@ -9,11 +9,11 @@ opt.relativenumber = true
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- Show NvDashboard when no buffers are open
-vim.api.nvim_create_autocmd("BufDelete", {
-  callback = function()
-    local bufs = vim.t.bufs
-    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
-      vim.cmd "Nvdash"
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufDelete", {
+--   callback = function()
+--     local bufs = vim.t.bufs
+--     if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+--       vim.cmd "Nvdash"
+--     end
+--   end,
+-- })
